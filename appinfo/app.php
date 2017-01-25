944edb2b8922154ca55bcb4a18c6b737f421bbce
Patrick Paysant <patrick.paysant@linagora.com>
2017-01-25 10:22:44 +0100

    App icon does not appear for non-(sub)admin users

--- appinfo/app.php
+++ appinfo/app.php
@@ -18,26 +18,36 @@ require_once __DIR__ . '/autoload.php';
 $app = new Application;
 $container = $app->getContainer();
 
-$container->query('\OCP\INavigationManager')->add(function () use ($container) {
-    $urlGenerator = $container->query('\OCP\IURLGenerator');
-    $l10n = $container->query('\OCP\IL10N');
-    return [
-        // the string under which your app will be referenced in owncloud
-        'id' => 'lotsofusers',
+$currentUser = \OC::$server->getUserSession()->getUser();
+$groupManager = \OC::$server->getGroupManager();
 
-        // sorting weight for the navigation. The higher the number, the higher
-        // will it be listed in the navigation
-        'order' => 10,
+$isAdminOrSubadmin = false;
+if (!empty($currentUser)) {
+    $isAdminOrSubadmin = $groupManager->getSubAdmin()->isSubAdmin($currentUser);
+}
 
-        // the route that will be shown on startup
-        'href' => $urlGenerator->linkToRoute('lotsofusers.page.users'),
+if ($isAdminOrSubadmin) {
+    $container->query('\OCP\INavigationManager')->add(function () use ($container) {
+        $urlGenerator = $container->query('\OCP\IURLGenerator');
+        $l10n = $container->query('\OCP\IL10N');
+        return [
+            // the string under which your app will be referenced in owncloud
+            'id' => 'lotsofusers',
 
-        // the icon that will be shown in the navigation
-        // this file needs to exist in img/
-        'icon' => $urlGenerator->imagePath('lotsofusers', 'app.svg'),
+            // sorting weight for the navigation. The higher the number, the higher
+            // will it be listed in the navigation
+            'order' => 10,
 
-        // the title of your application. This will be used in the
-        // navigation or on the settings page of your app
-        'name' => $l10n->t('Lots Of Users'),
-    ];
+            // the route that will be shown on startup
+            'href' => $urlGenerator->linkToRoute('lotsofusers.page.users'),
+
+            // the icon that will be shown in the navigation
+            // this file needs to exist in img/
+            'icon' => $urlGenerator->imagePath('lotsofusers', 'app.svg'),
+
+            // the title of your application. This will be used in the
+            // navigation or on the settings page of your app
+            'name' => $l10n->t('Lots Of Users'),
+        ];
 });
+}
